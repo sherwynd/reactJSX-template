92d5012f609fdf622ad7a1a1653c2d33d8f27c89
Sherwynd Liew <sherwynd123@gmail.com>
2024-04-01 02:59:46 +0800

    Added new components with json config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState, useEffect } from "react";
+import { Layout } from "./components/Layout";
+import { Test } from "./components/Test";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-import { ResponsiveDrawer } from "./components/Navbar";
+const theme = createTheme({});
 
 export default function App() {
   return (
     <>
-      <ResponsiveDrawer />
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Test />
+        </Layout>
+      </ThemeProvider>
     </>
   );
 }
---
+++ src/components/Layout.jsx
@@ -0,0 +1,12 @@
+// import makeStyles from "@mui/styles/makeStyles";
+import AppBar from "@mui/material/AppBar";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+const drawerWidth = 240;
+
+export function Layout({ children }) {
+  return <div>{children}</div>;
+}
