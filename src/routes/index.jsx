d3451149cd66442c9a05177fa6ddeb576123889a
KAE REN <hkr0528@gmail.com>
2024-04-24 13:55:17 +0800

    Added new payment page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -24,6 +24,7 @@ import { BlogPost } from "../pages/blog/Blog";
 import { Favorites } from "../pages/favourites/Favourites";
 import { Rate } from "../pages/rating/Rating";
 import { BlogDetails } from "../pages/blog/BlogDetail";
+import { CoachingCart } from "../pages/cart/CoachingCart.jsx";
 const routes = [
   {
     path: "/",
@@ -99,6 +100,10 @@ const routes = [
       {
         path: "cart",
         element: <Cart />,
+      },
+      {
+        path: "coachingCart",
+        element: <CoachingCart />,
       },
       {
         path: "favorites",
