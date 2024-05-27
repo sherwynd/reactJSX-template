111b6fd4e3039a4e28a258432d9c3fa9d0ecff0f
Sherwynd Liew <sherwynd123@gmail.com>
2024-05-27 18:38:29 +0800

    enchancement on all codes

--- src/contexts/NavbarProfileHistory.js
+++
@@ -1,7 +0,0 @@
-export const mainNavbarProfileHistory = [
-  { id: 0, label: "Product", route: "productHistory" },
-  { id: 1, label: "Blog", route: "blogHistory" },
-  { id: 2, label: "Coaching", route: "coachingHistory" },
-  { id: 3, label: "Rating", route: "ratingHistory" },
-  { id: 4, label: "Purchased", route: "purchaseHistory" },
-];
